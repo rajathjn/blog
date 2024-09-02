9f997d6227d9673fea2d282a495aae7815997f8f
rajathjn <rajathjnx@gmail.com>
2024-09-03 03:08:10 +0530

    chore: Update homepage features and landing page button text

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Side projects',
+    Svg: require('@site/static/img/developerActivity.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        I sometimes have a random flash of brilliance and try to create something cool.
+        I try my best to document them here or follow me on GitHub to see the latest project that I've made public 🧐.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
+    title: 'How I think',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        I am a firm believer that <b>Our Mind is for having Ideas not for Keeping them</b>.
+        I am someone who is always up for a challenge 💪.
+      </>
+    ),
+  },
+  {
+    title: 'Reach out to me',
+    Svg: require('@site/static/img/address_image.svg').default,
+    description: (
+      <>
+        If you have any queries or want to collaborate on something cool, feel free to reach out to me.
+        I am always open to new ideas and collaborations 🤝.
       </>
     ),
   },
