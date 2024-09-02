739ada35a92cc056a651bd99a579ff1b40c0f8b4
rajathjn <rajathjnx@gmail.com>
2024-09-02 19:51:01 +0530

    chore: Update repository and package names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://rajathjn.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blog/',
+  baseUrl: '/blogsite/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
